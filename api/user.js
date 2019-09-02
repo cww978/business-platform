442f8b639a84467deae0a1d69d71f597f71bbce9
cww978 <31878863+cww978@users.noreply.github.com>
2019-09-02 20:21:00 +0800

    添加二级菜单

--- api/user.js
+++ api/user.js
@@ -22,19 +22,19 @@ export function getUserMenus(){
         icon: 'https://gw.alipayobjects.com/zos/rmsportal/VBqNBOiGYkCjqocXjdUj.png',
         text: '常规数据接入',
         child: [
-          { text: '二支装品吸情况登记表', icon: ''},
-          { text: '库存盘点', icon: ''},
-          { text: '年度、季度、月度目标', icon: ''},
-          { text: '客户经理操作协同计划', icon: ''},
-          { text: '营销方案资源预报', icon: ''},
-          { text: '婚庆营销开展计划', icon: ''},
-          { text: '驻点品吸开展计划', icon: ''},
-          { text: '品牌文化宣讲开展计划', icon: ''},
-          { text: '营销方案申报', icon: ''},
-          { text: '营销方案分级审批', icon: ''},
-          { text: '营销方案评估', icon: ''},
-          { text: '营销方案评估审核', icon: ''},
-          { text: '营销方案评估抽查', icon: ''}
+          { text: '二支装品吸情况登记表', icon: '', path: '' },
+          { text: '库存盘点', icon: '', path: '' },
+          { text: '年度、季度、月度目标', icon: '', path: '' },
+          { text: '客户经理操作协同计划', icon: '', path: '' },
+          { text: '营销方案资源预报', icon: '', path: '' },
+          { text: '婚庆营销开展计划', icon: '', path: '' },
+          { text: '驻点品吸开展计划', icon: '', path: '' },
+          { text: '品牌文化宣讲开展计划', icon: '', path: '' },
+          { text: '营销方案申报', icon: '', path: '' },
+          { text: '营销方案分级审批', icon: '', path: '' },
+          { text: '营销方案评估', icon: '', path: '' },
+          { text: '营销方案评估审核', icon: '', path: '' },
+          { text: '营销方案评估抽查', icon: '', path: '' }
         ]
       },
       {
@@ -42,14 +42,14 @@ export function getUserMenus(){
         icon: 'https://gw.alipayobjects.com/zos/rmsportal/VBqNBOiGYkCjqocXjdUj.png',
         text: '工作写实类数据',
         child: [
-          { text: '驻点品吸写实数据录入', icon: ''},
-          { text: '烟模陈列写实数据录入', icon: ''},
-          { text: '微信群转发写实数据录入', icon: ''},
-          { text: '集赞活动配置', icon: ''},
-          { text: '集赞奖项配置', icon: ''},
-          { text: '集赞写实数据录入', icon: ''},
-          { text: '婚庆营销写实数据录入', icon: ''},
-          { text: '工作写实类数据录入完成进度查询', icon: ''}
+          { text: '驻点品吸写实数据录入', icon: '', path: '' },
+          { text: '烟模陈列写实数据录入', icon: '', path: '' },
+          { text: '微信群转发写实数据录入', icon: '', path: '' },
+          { text: '集赞活动配置', icon: '', path: '' },
+          { text: '集赞奖项配置', icon: '', path: '' },
+          { text: '集赞写实数据录入', icon: '', path: '' },
+          { text: '婚庆营销写实数据录入', icon: '', path: '' },
+          { text: '工作写实类数据录入完成进度查询', icon: '', path: '' }
         ]
       }
     ]
--- pages/home/child/child.js
+++ pages/home/child/child.js
@@ -3,6 +3,9 @@ Page({
   data: {
     menus: []
   },
+  clickMenu(e){
+    console.info('path', this.data.menus[e.index].path)
+  },
   onLoad(options) {
     dd.setNavigationBar({
       title: options.title  //修改title
